249bb7ee453f34d4def696005cded62aa807e844
mayuripandey <61756173+mayuripandey@users.noreply.github.com>
2020-12-27 19:06:37 +0100

    Add files via upload
    Calci code

---
+++ Calci.py
@@ -0,0 +1,13 @@
+num1 = float(input("enter first number : "))
+opp = input("enter opertor: ")
+num2 = float(input("enter secnd numbr: "))
+if  opp == "+":
+     print(num1 + num2)
+elif opp == "-":
+     print( num1 - num2)
+elif opp == "*":
+     print( num1 * num2 )
+elif opp == "/":
+     print(num1 / num2)
+else:
+     print("invalid")
